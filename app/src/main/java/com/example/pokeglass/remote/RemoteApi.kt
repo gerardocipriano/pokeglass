ca545066b8de08cad242dea24a6a77d5f18034c3
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-26 19:44:45 +0200

    added singleton for retrofit and moshi libreries

---
+++ app/src/main/java/com/example/pokeglass/remote/RemoteApi.kt
@@ -0,0 +1,23 @@
+package com.example.pokeglass.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RemoteApi {
+
+    private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    val service: PokeApiService = retrofit.create(PokeApiService::class.java)
+
+}
