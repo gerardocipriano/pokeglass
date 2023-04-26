eb6483c177a512defd0e645b21a3270fdf804bbe
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-26 21:29:53 +0200

    Added limit to just kanto pokedex

--- app/src/main/java/com/example/pokeglass/remote/PokeApiService.kt
+++ app/src/main/java/com/example/pokeglass/remote/PokeApiService.kt
@@ -11,7 +11,7 @@ import retrofit2.http.Query
 interface PokeApiService {
     @GET("pokemon")
     suspend fun getAllPokemon(
-        @Query("limit") limit: Int = 121,
+        @Query("limit") limit: Int = 151,
         @Query("offset") offset: Int = 0
     ): PokemonResponse
 
