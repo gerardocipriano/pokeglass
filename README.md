a4ec5e56bc102fee642b5f8224638ad08e670f90
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-04-26 15:57:36 +0200

    added main_menu

--- README.md
+++ README.md
@@ -31,7 +31,7 @@
 <br />
 <div align="center">
   <a href="https://github.com/gerardocipriano/pss22-pokeglass-android-app">
-    <img src="src/main/playstore-icon.png" alt="Logo" width="80" height="80">
+    <img src="src/main/res/playstore-icon.png" alt="Logo" width="80" height="80">
   </a>
 
 <h3 align="center">Pokè</h3>
--- app/src/main/java/com/example/pokeglass/MainActivity.kt
+++ app/src/main/java/com/example/pokeglass/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.pokeglass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_team -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
