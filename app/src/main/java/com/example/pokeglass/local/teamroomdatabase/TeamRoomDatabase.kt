f31ae81064cfc47c0e005a4c24b03adb47107f04
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-05-03 17:55:03 +0200

    Added Team Room DB

---
+++ app/src/main/java/com/example/pokeglass/local/teamroomdatabase/TeamRoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.pokeglass.local.teamroomdatabase
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.pokeglass.local.teamroomdatabase.daos.TeamDao
+import com.example.pokeglass.local.teamroomdatabase.entities.TeamEntity
+
+@Database(entities = [TeamEntity::class], version = 1)
+abstract class TeamRoomDatabase : RoomDatabase() {
+    abstract fun teamDao(): TeamDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: TeamRoomDatabase? = null
+
+        fun getDatabase(context: Context): TeamRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TeamRoomDatabase::class.java,
+                    "team_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
