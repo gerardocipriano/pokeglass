3fcbd9705cf2c7ed589a33b27190315d7fd8fa05
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-05-03 17:54:37 +0200

    Added TeamEntity

---
+++ app/src/main/java/com/example/pokeglass/local/teamroomdatabase/entities/TeamEntity.kt
@@ -0,0 +1,13 @@
+package com.example.pokeglass.local.teamroomdatabase.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "team_table")
+data class TeamEntity(
+    @PrimaryKey val name: String,
+    val hp: Int,
+    val attack: Int,
+    val defense: Int,
+    val speed: Int
+)
