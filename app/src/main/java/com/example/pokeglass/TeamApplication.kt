352b5c0dba4dea6f01fcaeb6d632df919c84ec65
gerardocipriano <gerardo.cipriano@studio.unibo.it>
2023-05-04 15:21:32 +0200

    Added TeamApplication

--- app/src/main/java/com/example/pokeglass/TeamApplication.kt
+++ app/src/main/java/com/example/pokeglass/TeamApplication.kt
@@ -8,6 +8,10 @@ import com.example.pokeglass.local.teamroomdatabase.TeamRoomDatabase
 class TeamApplication : Application() {
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    val database by lazy { TeamRoomDatabase.getDatabase(this) }
-    val repository by lazy { TeamRepository(RoomTeamLocalService(database.teamDao())) }
+    private val database by lazy { TeamRoomDatabase.getDatabase(this) }
+    private val repository by lazy { TeamRepository(RoomTeamLocalService(database.teamDao())) }
+
+    fun getTeamRepository(): TeamRepository {
+        return repository
+    }
 }
